e59babbbdcec9ae298728a2720a215430ab019ea
Lkaetsu <111883767+Lkaetsu@users.noreply.github.com>
2024-05-16 18:52:16 -0300

    Update main.c
    Main.c funcionando agora (com algumas limitações)

--- src/main.c
+++ src/main.c
@@ -6,20 +6,9 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-char** str_split(char *str){
-    char *cmd[3];
-    cmd[0] = str;
-    int j=1;
-    for(int i=0; i<strlen(str); i++){
-        if(str[i]==' '){
-            str[i] = '\0';
-            cmd[0][j++] = str[i+1];
-        }
-    }
-    return cmd;
+void cd(char* cmd){
+    chdir(cmd);
 }
-
-void cd(char* cmd);
 
 void path(char** cmd);
 
@@ -38,16 +27,45 @@ void execExtBin(char **cmd);
 
 int main()
 {
-    char path[50]="/";
-    char input[30];
+    int i;
+    char *dir;
+    char *input;
+    dir = (char*) calloc(100, sizeof(char *));
+    input = (char*) calloc(100, sizeof(char *));
     char *command[3];
-    while(strcmp(input, "exit")){
-        printf("%s>", path);
-        input = readline(NULL);
-        command = str_split(input);
-        if(strcmp(command[0], "cd")){
+    for(i=0;i<3;i++){
+        command[i] = (char*)calloc(30, sizeof(char *));
+    }
+    char* token;
+    while(1){
+        printf("%s>", getcwd(dir, 100));
+        strcpy(input, readline(NULL));
+        i=0;
+        token = strtok(input, " ");
+        while (token != NULL) {
+            strcpy(command[i], token);
+            i++;
+            token = strtok(NULL, " ");
+        }
+        if(strcmp(command[0], "cd")==0){
+            printf("cd");
             cd(command[1]);
         }
+        else if(strcmp(command[0], "path")==0){
+            path(command);
+        }
+        else if(strcmp(command[0], "exit")==0){
+            break;
+        }
+        else
+        {
+            execExtBin(command);
+        }
+    }
+    free(dir);
+    free(input);
+    for(int i=0;i<3;i++){
+        free(command[i]);
     }
     return 0;
 }
