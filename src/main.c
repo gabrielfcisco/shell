3c6880d5ccdc7e90b1a1c9db9815324a1c92ca08
Lkaetsu <leonardo.kaetsu9@gmail.com>
2024-05-21 23:28:18 -0300

    cd finalizado

--- src/main.c
+++ src/main.c
@@ -1,82 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <unistd.h>
-#include <readline/history.h>
+#include <sys/wait.h>
 #include <readline/readline.h>
+#include <readline/history.h>
+#include "../include/cd.h"
 #include "../include/path.h"
 
-
-void
-cd (char *cmd)
-{
-    chdir (cmd);
-}
-
-
-void execExtBin (char **cmd);
-// pid_t pid;
-// pid = fork();
-// if(pid<0){
-//     printf("Something went wrong.");
-//     exit(-1);
-// }
-// else if(pid==0){
-// }
-// else{
-//     wait(NULL);
-// }
-
-int
-main ()
+int main(int argc, char *argv[])
 {
     int i;
     char *dir;
     char *input;
-    dir = (char *)calloc (100, sizeof (char *));
-    input = (char *)calloc (100, sizeof (char *));
-    char *command[3];
-    for (i = 0; i < 3; i++)
-    {
-        command[i] = (char *)calloc (30, sizeof (char *));
+    char *command[2];
+    dir = (char*)calloc(512, sizeof(char *));
+    input = (char*)calloc(512, sizeof(char *));
+    for(i=0;i<2;i++){
+        command[i] = (char*)calloc(256, sizeof(char *));
     }
-    char *token;
-    while (1)
-    {
-        printf ("%s>", getcwd (dir, 100));
-        strcpy (input, readline (NULL));
-        i = 0;
-        token = strtok (input, " ");
-        while (token != NULL)
-        {
-            strcpy (command[i], token);
-            i++;
-            token = strtok (NULL, " ");
+    //fopen(argv[1], "r");
+    while(1){
+        printf("%s>", getcwd(dir, 512));
+        input = readline(NULL);
+        command[0] = strtok(input, " ");
+        command[1] = strtok(NULL, " ");
+        if(strcmp(command[0], "cd")==0){
+            cd(command[1]);
         }
-        if (strcmp (command[0], "cd") == 0)
-        {
-            printf ("cd");
-            cd (command[1]);
+        else if(strcmp(command[0], "path")==0){
+            path(command[1]);
         }
-        else if (strcmp (command[0], "path") == 0)
-        {
-            path (command[1]);
-        }
-        else if (strcmp (command[0], "exit") == 0)
-        {
+        else if(strcmp(command[0], "exit")==0){
             break;
         }
         else
         {
-            // execExtBin (command);
+            //execExtBin(command);
         }
     }
-    free (dir);
-    free (input);
-    for (int i = 0; i < 3; i++)
-    {
-        free (command[i]);
-    }
+    free(dir);
+    free(input);
+    free(command[1]);
     return 0;
 }
