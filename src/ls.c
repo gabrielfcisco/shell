900eb1258971d084035aab1eb54972eeecb9b6ff
gabrielfcisco <gabrifran017@gmail.com>
2024-05-22 23:18:26 -0300

    add: -l e -a ao ls

--- src/ls.c
+++ src/ls.c
@@ -1,59 +1,111 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 
 #define GREEN "\x1b[32m"
 #define BLUE "\x1b[34m"
 #define WHITE "\x1b[37m"
 
-void
-RecDir (char *path, int flag)
-{
-    DIR *dp = opendir (path);
-    if (!dp)
-    {
-        perror (path);
+void print_permissions(mode_t mode) {
+    char permissions[11];
+    strcpy(permissions, "----------");
+
+    if (S_ISDIR(mode)) permissions[0] = 'd';
+    if (mode & S_IRUSR) permissions[1] = 'r';
+    if (mode & S_IWUSR) permissions[2] = 'w';
+    if (mode & S_IXUSR) permissions[3] = 'x';
+    if (mode & S_IRGRP) permissions[4] = 'r';
+    if (mode & S_IWGRP) permissions[5] = 'w';
+    if (mode & S_IXGRP) permissions[6] = 'x';
+    if (mode & S_IROTH) permissions[7] = 'r';
+    if (mode & S_IWOTH) permissions[8] = 'w';
+    if (mode & S_IXOTH) permissions[9] = 'x';
+
+    printf("%s ", permissions);
+}
+
+void print_detailed_info(const char *path, const struct dirent *ep) {
+    char fullpath[512];
+    struct stat st;
+    struct passwd *pw;
+    struct group *gr;
+    struct tm *tm;
+    char datestring[256];
+
+    sprintf(fullpath, "%s/%s", path, ep->d_name);
+    stat(fullpath, &st);
+
+    print_permissions(st.st_mode);
+    printf("%ld ", st.st_nlink);
+
+    pw = getpwuid(st.st_uid);
+    printf("%s ", pw->pw_name);
+
+    gr = getgrgid(st.st_gid);
+    printf("%s ", gr->gr_name);
+
+    printf("%5ld ", st.st_size);
+
+    tm = localtime(&st.st_mtime);
+    strftime(datestring, sizeof(datestring), "%b %d %H:%M", tm);
+    printf("%s ", datestring);
+
+    printf("%s\n", ep->d_name);
+}
+
+void RecDir(char *path, int recursive, int include_hidden, int detailed) {
+    DIR *dp = opendir(path);
+    if (!dp) {
+        perror(path);
         return;
     }
     struct dirent *ep;
     char newdir[512];
-    printf (BLUE "\n%s :\n" WHITE, path);
-    while ((ep = readdir (dp)))
-        if (strncmp (ep->d_name, ".", 1))
-            printf (GREEN "\t%s\n" WHITE, ep->d_name);
-    closedir (dp);
-    dp = opendir (path);
-    while ((ep = readdir (dp)))
-        if (strncmp (ep->d_name, ".", 1))
-        {
-            if (flag && ep->d_type == 4)
-            {
-                sprintf (newdir, "%s/%s", path, ep->d_name);
-                RecDir (newdir, 1);
-            }
+    printf(BLUE "%s :\n" WHITE, path);
+    while ((ep = readdir(dp))) {
+        if (!include_hidden && strncmp(ep->d_name, ".", 1) == 0) {
+            continue;
         }
-    closedir (dp);
+        if (detailed) {
+            print_detailed_info(path, ep);
+        } else {
+            printf("%s   ", ep->d_name);
+        }
+    }
+    closedir(dp);
+    dp = opendir(path);
+    while ((ep = readdir(dp))) {
+        if (!include_hidden && strncmp(ep->d_name, ".", 1) == 0) {
+            continue;
+        }
+        if (recursive && ep->d_type == DT_DIR && strncmp(ep->d_name, ".", 1) != 0) {
+            sprintf(newdir, "%s/%s", path, ep->d_name);
+            RecDir(newdir, recursive, include_hidden, detailed);
+        }
+    }
+    closedir(dp);
 }
 
-int
-main (int argc, char **argv)
-{
-    switch (argc)
-    {
-    case 2:
-        RecDir (argv[1], 0);
-        break;
-    case 3:
-        if (strcmp (argv[1], "-R") == 0)
-            RecDir (argv[2], 1);
-        else
-        {
-            printf ("Uso de argumento errado");
-            return 1;
-            break;
+int main(int argc, char **argv) {
+    int recursive = 0, include_hidden = 0, detailed = 0;
+    char *path = ".";
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-R") == 0) {
+            recursive = 1;
+        } else if (strcmp(argv[i], "-a") == 0) {
+            include_hidden = 1;
+        } else if (strcmp(argv[i], "-l") == 0) {
+            detailed = 1;
+        } else {
+            path = argv[i];
         }
-    default:
-        RecDir (".", 0);
     }
+
+    RecDir(path, recursive, include_hidden, detailed);
     return 0;
 }
