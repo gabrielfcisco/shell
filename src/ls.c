9fc7e53022c44874fbe2e5af0a230348b7a995a6
gabrielfcisco <gabrifran017@gmail.com>
2024-05-22 21:54:01 -0300

    create: ls

---
+++ src/ls.c
@@ -0,0 +1,56 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+
+#define GREEN "\x1b[32m"
+#define BLUE "\x1b[34m"
+#define WHITE "\x1b[37m"
+
+void
+RecDir (char *path, int flag)
+{
+    DIR *dp = opendir (path);
+    if (!dp)
+    {
+        perror (path);
+        return;
+    }
+    struct dirent *ep;
+    char newdir[512];
+    printf (BLUE "\n%s :\n" WHITE, path);
+    while ((ep = readdir (dp)))
+        if (strncmp (ep->d_name, ".", 1))
+            printf (GREEN "\t%s\n" WHITE, ep->d_name);
+    closedir (dp);
+    dp = opendir (path);
+    while ((ep = readdir (dp)))
+        if (strncmp (ep->d_name, ".", 1))
+        {
+            if (flag && ep->d_type == 4)
+            {
+                sprintf (newdir, "%s/%s", path, ep->d_name);
+                RecDir (newdir, 1);
+            }
+        }
+    closedir (dp);
+}
+
+int
+main (int argc, char **argv)
+{
+    switch (argc)
+    {
+    case 2:
+        RecDir (argv[1], 0);
+        break;
+    case 3:
+        if (strcmp (argv[1], "-R") == 0)
+            RecDir (argv[2], 1);
+        else
+            Usage ();
+        break;
+    default:
+        RecDir (".", 0);
+    }
+    return 0;
+}
